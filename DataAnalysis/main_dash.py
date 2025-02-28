1d20d14621c47cb236ef3b9666fbcb89351f6055
iadel93 <engsmsm@gmail.com>
2025-02-28 10:33:57 +0200

    dash

--- DataAnalysis/main_dash.py
+++ DataAnalysis/main_dash.py
@@ -3,33 +3,72 @@
 
 
 from dash import Dash, html, dcc
+import dash
 import plotly.express as px
 import pandas as pd
 
 app = Dash(__name__)
+df = pd.DataFrame(
+    {
+        "Fruits": ["Apple", "Banana", "Orange", "Apple", "Banana", "Orange"],
+        "Amount": [4, 3, 1, 2, 2, 1],
+        "City": ["New York City", "New York City", "New York City", "Montreal", "Montreal", "Montreal"]
+    }
+)
+multi_select = dcc.Dropdown(
+    id='multi-select-dropdown',
+    options=[{'label': i, 'value': i} for i in df['Fruits'].unique()],
+    value=['Apple', 'Orange'],
+    multi=True
+)
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
+@app.callback(
+    dash.dependencies.Output('pie-chart', 'figure'),
+    [dash.dependencies.Input('multi-select-dropdown', 'value')]
+)
+def update_pie_chart(selected_fruits):
+    filtered_df = df[df['Fruits'].isin(selected_fruits)]
+    fig = px.pie(filtered_df, values='Amount', names='Fruits', title='Fruit Consumption per City')
+    return fig
 
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+fig = px.pie(df, values='Amount', names='Fruits', title='Fruit Consumption per City')
 
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+app.layout = html.Div([
+    html.Div(children=[
 
-    html.Div(children='''
-        Dash: A web application framework for your data.
-    '''),
+        html.Br(),
+        html.Label('Multi-Select Dropdown'),
+        multi_select,
 
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
-    )
-])
+        html.Br(),
+        html.Label('Radio Items'),
+        dcc.RadioItems(['New York City', 'Montréal', 'San Francisco'], 'Montréal'),
+    ], style={'padding': 10, 'flex': 1}),
+
+    html.Div(children=[
+        html.Label('Checkboxes'),
+        dcc.Checklist(['New York City', 'Montréal', 'San Francisco'],
+                      ['Montréal', 'San Francisco']
+        ),
+
+        html.Br(),
+        html.Label('Text Input'),
+        dcc.Input(value='MTL', type='text'),
+
+        html.Br(),
+        html.Label('Slider'),
+        dcc.Slider(
+            min=0,
+            max=9,
+            marks={i: f'Label {i}' if i == 1 else str(i) for i in range(1, 6)},
+            value=5,
+        ),
+
+        html.Br(),
+        html.Label('Pie Chart'),
+        dcc.Graph(figure=fig, id='pie-chart')
+    ], style={'padding': 10, 'flex': 1})
+], style={'display': 'flex', 'flexDirection': 'row'})
 
 if __name__ == '__main__':
     app.run(debug=True)
